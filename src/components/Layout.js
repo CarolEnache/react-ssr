b12cfb394d9bb8a05d4726f8b3cd952026c7f920
Carol Enache <enache.carol@gmail.com>
2018-05-29 23:20:15 +0100

    react-router

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Link, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
 
 export default class Layout extends Component {
     constructor(){
@@ -13,6 +17,16 @@ export default class Layout extends Component {
         return (
             <div>
                 <h1>{this.state.title}</h1>
+                <div>
+                    <Link to='/'>Home</Link>
+                    <Link to='/About'>About</Link>
+                    <Link to='/Contact'>Contact</Link>                
+                </div>
+                <Switch>
+                    <Route path='/' exact componet={ Home } />
+                    <Route path='/about' exact componet={ About } />
+                    <Route path='/contact' exact componet={ Contact } />
+                </Switch>
             </div>
         );
     }
